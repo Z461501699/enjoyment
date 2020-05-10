22b692286c4f09f7b15e54c7d8f340faf33ee28e
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-10 16:46:00 +0800

    列表标题组件

---
+++ components/list-title/index.js
@@ -0,0 +1,30 @@
+// components/list-title/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 标题信息
+    title: {
+      value: '列表',
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    more() {
+      this.triggerEvent('click')
+
+    }
+  }
+})
