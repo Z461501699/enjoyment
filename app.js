a17b74afba2d678d565205dd45f7d12f1eef00c9
brotherfish <516563685@qq.com>
2020-05-12 19:14:42 +0800

    个人中心框架搭建及底部菜单图标添加

--- app.js
+++ app.js
@@ -7,4 +7,21 @@ App({
   request: function (apiKey, data = {}, loadingMessage) {
     return request(this, apiKey, data, loadingMessage);
   },
+
+  /**
+   * 执行用户登录
+   */
+  doLogin(delta) {
+    // 保存当前页面
+    let pages = getCurrentPages();
+    if (pages.length) {
+      let currentPage = pages[pages.length - 1];
+      "pages/login/login" != currentPage.route &&
+        wx.setStorageSync("currentPage", currentPage);
+    }
+    // 跳转授权页面
+    wx.navigateTo({
+      url: "/pages/login/login?delta=" + (delta || 1)
+    });
+  }
 })
