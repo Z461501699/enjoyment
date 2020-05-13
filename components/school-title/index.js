6fda469fae3c3e2a0803210210f9c5d7a4b2973e
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-14 01:08:55 +0800

    增加学校title组件

---
+++ components/school-title/index.js
@@ -0,0 +1,26 @@
+// components/school-title/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      value: '标题',
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
