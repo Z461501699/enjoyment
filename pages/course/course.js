0037ee6a72a1464d1bd73ecb534b16204438544d
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-13 22:30:52 +0800

    课程组件添加模拟数据

--- pages/course/course.js
+++ pages/course/course.js
@@ -5,18 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    courseList: [],
+    pageSize: 10,
+    pageNo: 1,
+    isLoadAll: false
   },
-    // 搜索功能
-    search: function ({detail}) {
-      console.log('e', detail)
-    },
-  
-
+  // 搜索功能
+  search: function ({
+    detail
+  }) {
+    console.log('e', detail)
+  },
+  // 获取课程数据
+  getCourseList() {
+    const that = this
+    wx.showLoading({
+      title: '加载中。。。',
+      mask: true
+    })
+    const {
+      courseList
+    } = that.data
+    courseList.push('', '', '', '', '', '', '', '', '', '')
+    setTimeout(function () {
+      that.setData({
+        courseList
+      })
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    }, 1500)
+  },
+  /**
+   * 跳转课程详情
+   */
+  onToCourseDetail(e) {
+    console.log(e.detail.id)
+    wx.navigateTo({
+      url: '/pages/courseDetail/courseDetail',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCourseList()
 
   },
 
@@ -52,13 +84,18 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    const that = this
+    that.setData({
+      courseList: []
+    })
+    that.getCourseList()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    this.getCourseList()
 
   },
 
