633d9160d8260f4931a2e939799c1c2aea6265b6
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-15 23:38:54 +0800

    添加配置文件

---
+++ config/commonData.js
@@ -0,0 +1,7 @@
+export const MESSAGE_OPTIONS = [
+{title: '系统反馈',color: '#1A7CFC',url: '/images/icon/icon_msg_setting.png'},
+{title: '清单信息',color: '#12AE85',url: '/images/icon/icon_msg_order.png'},
+{title: '退款申请',color: '#FB8D32',url: '/images/icon/icon_msg_rmb.png'},
+{title: '系统信息',color: '#F488C0',url: '/images/icon/icon_msg_notice.png'}
+
+]
