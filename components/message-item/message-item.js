47ff8d461fdf2585f088383a2c4bfb11b44161f8
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-15 23:37:42 +0800

    添加消息组件

---
+++ components/message-item/message-item.js
@@ -0,0 +1,29 @@
+// components/message/message-item.js
+import {
+  MESSAGE_OPTIONS
+} from "../../config/commonData"
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    type: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    MESSAGE_OPTIONS
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
