b861e5deaa593d708043ce05588cffaf77d18fbe
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-17 19:01:58 +0800

    增加反馈页面

---
+++ pages/coupleBack/coupleBack.js
@@ -0,0 +1,114 @@
+// pages/coupleBack/coupleBack.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    fileList: []
+  },
+  onChange(event) {
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    Toast(`当前值：${value}, 当前索引：${index}`);
+  },
+  // 获取上传的图片
+  getImages(e) {
+    const {
+      detail
+    } = e
+    const {
+      file
+    } = detail
+    const {
+      fileList
+    } = this.data
+    console.log('获取图片', file);
+    file.forEach(el => {
+      fileList.push(el)
+    })
+    this.setData({
+      fileList
+    })
+  },
+  // 删除图片
+  handleDelImage(e) {
+    console.log('del image', e);
+    const {
+      detail
+    } = e
+    const {
+      index
+    } = detail
+    const {
+      fileList
+    } = this.data
+    fileList.splice(index, 1)
+    this.setData({
+      fileList
+    })
+  },
+  // 提交表单
+  handleSubmit() {
+    console.log('提交表单');
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
