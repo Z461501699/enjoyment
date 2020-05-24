d3e86ea32119bc388e117cbc89c89047a0885a30
kingzhang1228 <38022406+kingzhang1228@users.noreply.github.com>
2020-05-24 19:44:26 +0800

    先加退款页面

---
+++ pages/salesReturnDetail/salesReturnDetail.js
@@ -0,0 +1,79 @@
+// pages/salesReturnDetail/salesReturnDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    steps: [
+    {
+      text: '退款成功',
+      desc: '2020-05-24',
+    },
+    {
+      text: '银行处理',
+      desc: '2020-05-22',
+    },
+    {
+      text: '发起退款',
+      desc: '2020-05-21',
+    },
+  ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
