0e577b406eb3c4acccd8f20f42371350f45fe869
kingzhang1228 <zhang_xxyy66@163.com>
2020-07-24 22:23:07 +0800

    refactor：修改api请求地址

--- common/config.js
+++ common/config.js
@@ -8,7 +8,7 @@
 // 生产状态
 export const ProductStatus = false;
 // 测试环境
-const HostTest = 'http://52.81.65.217:8083';
+const HostTest = 'http://114.67.205.242:8083';
 // 生产环境
 const HostProduct = '';
 // 域名
